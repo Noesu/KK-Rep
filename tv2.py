ff56ff92d3e2fc418d4a0c26fc159573694ef6ce
Anton Petrov <anton.petrov.swe@gmail.com>
2023-07-09 00:37:03 +0100

    Refactoring suggestion

--- tv2.py
+++ tv2.py
@@ -10,69 +10,75 @@
 # Программа должна следить за тем, чтобы
 # номер канала и уровень громкости оставались в допустимых пределах.
 
-channel_list = {1: "News channel",
-                2: "Music channel",
-                3: "Cartoon channel"}  # Список каналов
+from enum import Enum, IntEnum
+from typing import Optional
 
 
-class Tv(object):  # Заголовок класса
-    """Пульт управления"""
-    status = "off"
-    volume = 1
-    channel = 1  # Задание исходных значений
+MIN_VOLUME_LEVEL = 0
+MAX_VOLUME_LEVEL = 5
 
-    # В методе предоставления еще раззадаю значения по умолчанию,
-    # правда не понимаю зачем, но иначе не работает )))
-    def __init__(self, status="off", channel=1, volume=1):
-        self.status = status
-        self.channel = channel
+MIN_CHANNEL_NUMBER = 1
+MAX_CHANNEL_NUMBER = 3
+
+
+class TVPowerStatus(str, Enum):
+    POWER_ON = "ON"
+    POWER_OFF = "OFF"
+
+
+class TVButton(str, Enum):
+    CHANNEL_UP = "CHANNEL_UP"
+    CHANNEL_DOWN = "CHANNEL_DOWN"
+
+    VOLUME_UP = "VOLUME_UP"
+    VOLUME_DOWN = "VOLUME_DOWN"
+
+
+class TVMenuOption(IntEnum):
+    SWITCH_POWER = 0
+    CHANNEL_UP = 1
+    CHANNEL_DOWN = 2
+    VOLUME_UP = 3
+    VOLUME_DOWN = 4
+
+    @classmethod
+    def from_user_input(cls, user_input: str) -> Optional["TVMenuOption"]:
+        try:
+            return TVMenuOption(int(user_input))
+        except ValueError:
+            return None
+
+
+class TVChannelNumber(IntEnum):
+    NEWS = 1
+    MUSIC = 2
+    CARTOON = 3
+
+
+CHANNEL_NUMBER_TO_NAME = {
+    TVChannelNumber.NEWS: "News channel",
+    TVChannelNumber.MUSIC: "Music channel",
+    TVChannelNumber.CARTOON: "Cartoon channel",
+}
+
+
+class TVRemote:
+    """Пульт управления ТВ"""
+
+    def __init__(
+        self,
+        power_status: TVPowerStatus,
+        channel_number: TVChannelNumber,
+        volume: int,
+    ):
+        self.power_status = power_status
+        self.channel_number = channel_number
         self.volume = volume
 
-    def __str__(self):  # Формирование статусной строки
-        channel_definition = channel_list[self.channel]
-        textline = "TV is " + self.status
-        textline += "\tChannel " + str(self.channel)
-        textline += "\t" + channel_definition
-        textline += "\tVolume " + str(self.volume)
-        return textline
-
-    def status_changer(self):  # Кнопка включения и выключения
-        if self.status == "on":
-            self.status = "off"  # Выключение телевизора если он включен
-            print("TV switched", self.status)
-        else:
-            self.status = "on"  # Включение телевизора если он выключен
-            print("TV switched", self.status)
-
-    def channel_operation(self, channel_switch):  # Панель кнопок переключения каналов вверх и вниз
-        if channel_switch == "up":
-            if self.channel < 3:
-                self.channel += 1  # Переключение канала вверх
-            else:
-                print("This is the last channel")  # Отбойник переключения вверх если последний канал
-        elif channel_switch == "down":
-            if self.channel > 1:
-                self.channel -= 1  # Переключение канала вниз
-            else:
-                print("This is the first channel")  # Отбойник переключения вниз если первый канал
-
-    def volume_operation(self, volume_switch):  # Панель кнопок переключения громкости вверх и вниз
-        if volume_switch == "up":
-            if self.volume < 5:
-                self.volume += 1  # Добавление громкости
-            else:
-                print("Max volume")  # Отбойник добавления громкости если маскимум
-        elif volume_switch == "down":
-            if self.volume > 0:
-                self.volume -= 1  # Уменьшение громкости
-            else:
-                print("Mute")  # Отбойник добавления громкости если минимум
-
-
-def main():
-    tv = Tv("off")
-    while True:
-        print("""
+    @property
+    def menu(self) -> str:
+        return (
+            """
             TV Operation
 
             0 - Switch on/off
@@ -80,46 +86,100 @@ def main():
             2 - Channel down
             3 - Volume up
             4 - Volume down
-            """)  # Вывод меню телевизора
-        print(tv)  # Вывод статуса телевизора
-        choice = input("Choice: ")
+            """
+        )
+
+    @property
+    def state(self) -> str:
+        return (
+            f"TV is {self.power_status}\t "
+            f"Channel {self.channel_number}\t "
+            f"{CHANNEL_NUMBER_TO_NAME[self.channel_number]}\t "
+            f"Volume {self.volume}"
+        )
+
+    def switch_power(self) -> None:
+        if self.power_status == TVPowerStatus.POWER_ON:
+            self.power_status = TVPowerStatus.POWER_OFF
+        else:
+            self.power_status = TVPowerStatus.POWER_ON
+
+        print(f"TV switched {self.power_status}")
+
+    def switch_channel(self, button: TVButton):
+        if button == TVButton.CHANNEL_UP:
+            if self.channel_number < MAX_CHANNEL_NUMBER:
+                self.channel_number += 1
+            else:
+                print("This is the last channel")
+
+        elif button == TVButton.CHANNEL_DOWN:
+            if self.channel_number > MIN_CHANNEL_NUMBER:
+                self.channel_number -= 1
+            else:
+                print("This is the first channel")
+
+    def switch_volume(self, button: TVButton):
+        if button == TVButton.VOLUME_UP:
+            if self.volume < MAX_VOLUME_LEVEL:
+                self.volume += 1
+            else:
+                print("Max volume")
+
+        elif button == TVButton.VOLUME_DOWN:
+            if self.volume > MIN_VOLUME_LEVEL:
+                self.volume -= 1
+            else:
+                print("Mute")
+
+
+def main():
+    tv_remote = TVRemote(
+        power_status=TVPowerStatus.POWER_OFF,
+        channel_number=TVChannelNumber.NEWS,
+        volume=MIN_VOLUME_LEVEL + 1,
+    )
+
+    running = True
+    while running:
+        print(tv_remote.menu)
+        print(tv_remote.state)
+
+        user_input = input("Choice: ")
+
+        menu_option = TVMenuOption.from_user_input(user_input)
+        if menu_option is None:
+            print(f"\nSorry, but '{user_input}' isn't a valid menu option.")
+            continue
 
         # Switch on/off
-        if choice == "0":
-            tv.status_changer()
+        if menu_option == TVMenuOption.SWITCH_POWER:
+            tv_remote.switch_power()
+
+        elif menu_option in TVMenuOption:
+            if tv_remote.power_status == TVPowerStatus.POWER_OFF:
+                print("No reaction, TV is off")
+                continue
 
         # Channel up
-        elif choice == "1":
-            if tv.status == "on":
-                tv.channel_operation("up")
-            else:
-                print("No reaction, TV os off")
+        if menu_option == TVMenuOption.CHANNEL_UP:
+
+            tv_remote.switch_channel(button=TVButton.CHANNEL_UP)
 
         # Channel down
-        elif choice == "2":
-            if tv.status == "on":
-                tv.channel_operation("down")
-            else:
-                print("No reaction, TV os off")
+        elif menu_option == TVMenuOption.CHANNEL_DOWN:
+            tv_remote.switch_channel(button=TVButton.CHANNEL_DOWN)
 
         # Volume up
-        elif choice == "3":
-            if tv.status == "on":
-                tv.volume_operation("up")
-            else:
-                print("No reaction")
+        elif menu_option == TVMenuOption.VOLUME_UP:
+            tv_remote.switch_volume(button=TVButton.VOLUME_UP)
 
         # Volume down
-        elif choice == "4":
-            if tv.status == "on":
-                tv.volume_operation("down")
-            else:
-                print("No reaction")
+        elif menu_option == TVMenuOption.VOLUME_DOWN:
+            tv_remote.switch_volume(button=TVButton.VOLUME_DOWN)
 
-        # some unknown choice
-        else:
-            print("\nSorry, but", choice, "isn't a valid choice.")
+    input("\n\nPress the enter key to exit.")
 
 
-main()
-input("\n\nPress the enter key to exit.")
+if __name__ == '__main__':
+    main()
